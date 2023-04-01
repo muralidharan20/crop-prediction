04538724c64991a53119bcd0b13b543429ed90f6
muralidharan20 <70807333+muralidharan20@users.noreply.github.com>
2023-04-01 18:49:01 +0530

    Update urls.py

--- cropprediction/urls.py
+++ cropprediction/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from .views import crop_analyze,util_data
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/analyze/',crop_analyze),
-    path('api/util/',util_data)
+    path('api/',include('api.urls')),
 ]
