0c46a41d0a6798121e50b416070237a417a0337f
muralidharan20 <70807333+muralidharan20@users.noreply.github.com>
2023-04-02 11:39:37 +0530

    Update views.py

--- api/views.py
+++ api/views.py
@@ -5,6 +5,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.views import APIView
+from .serializers import CropPredictionSerializers
+from rest_framework.response import Response
+from rest_framework import status
 
 @csrf_exempt
 def util_data(request):
@@ -30,20 +34,19 @@ def util_data(request):
 	joblib.dump(clf, "static/pkl/crop_classifier.pkl")
 	return JsonResponse({"Accuracy":accuracy},safe=False)
 
-@csrf_exempt
-def crop_analyze(request):
-    if request.method == 'POST':
-        clf = joblib.load('static/pkl/crop_classifier.pkl')
-
-        N_SOIL = request.POST['N_SOIL']
-        P_SOIL = request.POST['P_SOIL']
-        K_SOIL = request.POST['K_SOIL']
-        TEMPARATURE = request.POST['TEMPARATURE']
-        HUMIDITY = request.POST['HUMIDITY']
-        PH = request.POST['PH']
-        RAINFALL = request.POST['RAINFALL']
-
-        X = pd.DataFrame([[N_SOIL,P_SOIL,K_SOIL,TEMPARATURE,HUMIDITY,PH,RAINFALL]],columns=["N_SOIL","P_SOIL","K_SOIL","TEMPERATURE","HUMIDITY","ph","RAINFALL"])
-        prediction = {"prediction":clf.predict(X)[0]}
-
-        return JsonResponse(prediction,safe=False)
+class CropPredictionView(APIView):
+	def post(self,request,format=None):
+		serializers = CropPredictionSerializers(data=request.data)
+		if serializers.is_valid():
+			clf = joblib.load('static/pkl/crop_classifier.pkl')
+			N_SOIL = request.data.get('N_SOIL')
+			P_SOIL = request.data.get('P_SOIL')
+			K_SOIL = request.data.get('K_SOIL')
+			TEMPARATURE = request.data.get('TEMPARATURE')
+			HUMIDITY = request.data.get('HUMIDITY')
+			PH = request.data.get('PH')
+			RAINFALL = request.data.get('RAINFALL')
+			X = pd.DataFrame([[N_SOIL,P_SOIL,K_SOIL,TEMPARATURE,HUMIDITY,PH,RAINFALL]],columns=["N_SOIL","P_SOIL","K_SOIL","TEMPERATURE","HUMIDITY","ph","RAINFALL"])
+			prediction = {"prediction":clf.predict(X)[0]}
+			return Response(prediction,status=status.HTTP_200_OK)
+		return Response(serializers.errors,status=status.HTTP_500_INTERNAL_SERVER_ERROR)
